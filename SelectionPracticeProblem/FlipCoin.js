5c092bc692e6ec7ea3e4145e1c492077e0466e9d
Diksha Trivedi <112503942+DIKSHATRIVEDI@users.noreply.github.com>
2025-03-19 12:04:08 +0530

    Delete SelectionPracticeProblem directory

--- SelectionPracticeProblem/FlipCoin.js
+++
@@ -1,9 +0,0 @@
-// Function to simulate coin flip
-function flipCoin() {
-    let result = Math.floor(Math.random() * 2); // 0 or 1
-    return result === 0 ? "Heads" : "Tails";
-}
-
-// Flip the coin
-let outcome = flipCoin();
-console.log(`Coin Flip Result: ${outcome}`);
